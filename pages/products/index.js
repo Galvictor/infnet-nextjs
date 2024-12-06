4d9b1307bc75e9f5713196b48ff0cfe91fdb8602
jlopes <joao.victor@lojainterativa.com>
2024-12-05 21:03:23 -0300

    ajustes

--- pages/products/index.js
+++ pages/products/index.js
@@ -19,40 +19,41 @@ export default function ProductsPage({products}) {
         <div className="container top-navbar">
             <h1 className="text-2xl font-bold">Produtos</h1>
             <p className="text-gray-500">Aqui você encontra os melhores produtos do mercado!</p>
-        </div>
-        <div className="row">
-            {products.map((product) => (
-                <div className="col-2 mb-4" key={product.id}>
-                    <div className="card h-100 shadow-sm">
-                        <Image width={500}
-                               height={500} src={product.thumbnail} className="card-img-top img-fluid"
-                               alt={product.title}/>
-                        <div className="card-body">
-                            <h5 className="card-title text-truncate">{product.title}</h5>
-                            <p className="card-text text-muted text-truncate">{product.description}</p>
 
-                            {/* Avaliação */}
-                            <div className="d-flex align-items-center mb-3">
-                                <div className="stars me-2">{renderStars(product.rating)}</div>
-                                <span className="text-muted">({product.rating.toFixed(1)})</span>
-                            </div>
+            <div className="row">
+                {products.map((product) => (
+                    <div className="col-4 mb-4" key={product.id}>
+                        <div className="card h-100 shadow-sm">
+                            <Image width={500}
+                                   height={500} src={product.thumbnail} className="card-img-top img-fluid"
+                                   alt={product.title}/>
+                            <div className="card-body">
+                                <h5 className="card-title text-truncate">{product.title}</h5>
+                                <p className="card-text text-muted text-truncate">{product.description}</p>
 
-                            {/* Preços */}
-                            <div className="mb-3">
+                                {/* Avaliação */}
+                                <div className="d-flex align-items-center mb-3">
+                                    <div className="stars me-2">{renderStars(product.rating)}</div>
+                                    <span className="text-muted">({product.rating.toFixed(1)})</span>
+                                </div>
+
+                                {/* Preços */}
+                                <div className="mb-3">
                                 <span
                                     className="old-price text-decoration-line-through text-muted me-2">${product.price.toFixed(2)}</span>
-                                <span
-                                    className="price text-success fw-bold">{renderPriceDiscount(product.price, product.discountPercentage)}</span>
-                            </div>
+                                    <span
+                                        className="price text-success fw-bold">{renderPriceDiscount(product.price, product.discountPercentage)}</span>
+                                </div>
 
-                            {/* Botão */}
-                            <Link href={`/products/${product.id}`} className="btn btn-primary w-100">
-                                Ver Detalhes
-                            </Link>
+                                {/* Botão */}
+                                <Link href={`/products/${product.id}`} className="btn btn-primary w-100">
+                                    Ver Detalhes
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     </Layout>
 }
